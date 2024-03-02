d7a8ddae7bf26a82de13faa572a6241d7db28478
kaan-k <63565880+kaan-k@users.noreply.github.com>
2024-03-02 13:17:26 +0300

    Initial Commit

---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnetweb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
